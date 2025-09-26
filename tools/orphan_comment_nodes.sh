62f3f9d2ef1cc231b794e2f4d89ee24faa5b224f
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-09-26 17:50:51 -0400

    fixes to orphan nodes

--- tools/orphan_comment_nodes.sh
+++ tools/orphan_comment_nodes.sh
@@ -1,12 +1,22 @@
+# sqlite3 a.db <<EOM
+# .mode line
+# SELECT day, title, logid, substr(tag,4) as nodename FROM logtags
+# INNER join logs on logs.rowid = logtags.logid
+# WHERE nodename NOT in
+# (SELECT substr(tag,4) as nodename FROM logtags
+# JOIN dz_nodes on dz_nodes.name = nodename
+# WHERE
+# tag LIKE 'dz:%')
+# AND tag LIKE 'dz:%'
+# ;
+# EOM
 sqlite3 a.db <<EOM
 .mode line
-SELECT day, title, logid, substr(tag,4) as nodename FROM logtags
-INNER join logs on logs.rowid = logtags.logid
-WHERE nodename NOT in
-(SELECT substr(tag,4) as nodename FROM logtags
-JOIN dz_nodes on dz_nodes.name = nodename
-WHERE
-tag LIKE 'dz:%')
-AND tag LIKE 'dz:%'
+
+SELECT day, title, lz_connections.id, node FROM lz_connections
+INNER join lz_entries on lz_entries.id = lz_connections.id
+WHERE node NOT in
+(SELECT node FROM lz_connections
+JOIN dz_nodes on dz_nodes.name = node)
 ;
 EOM

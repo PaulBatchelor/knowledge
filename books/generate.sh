baafb2605de3d308fad526be13c66b19340f6c35
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-10-12 18:11:49 -0400

    ink

--- books/generate.sh
+++ books/generate.sh
@@ -3,6 +3,6 @@ do
     BOOK=$(echo $LINE | cut -d ':' -f 1)
     QUERY=$(echo $LINE | cut -d ':' -f 2)
     echo $BOOK $QUERY
-    ./tools/inkbook.sh $QUERY > book/$BOOK.tex
-done < book/books.txt
+    ./tools/inkbook.sh $QUERY > books/$BOOK.tex
+done < books/books.txt
 
--- books/render.sh
+++ books/render.sh
@@ -3,6 +3,6 @@ do
     BOOK=$(echo $LINE | cut -d ':' -f 1)
     QUERY=$(echo $LINE | cut -d ';' -f 2)
     echo $BOOK
-    cd book; pdftex $BOOK; cd -
-done < book/books.txt
+    cd books; pdftex $BOOK; cd -
+done < books/books.txt
 

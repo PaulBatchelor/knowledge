1bd6d7f591237cd70b89a382edac7d02308632b7
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-10-17 21:41:56 -0400

    cs342 studying

--- dz/csmet/TODO.sql
+++ dz/csmet/TODO.sql
@@ -1,4 +1,4 @@
-.mode tabs
+.mode lines
 WITH
 slides as (SELECT id FROM dz_nodes
 WHERE name like 'csmet/cs342/slides/%'),
@@ -7,10 +7,17 @@ slide_connections as (SELECT left, right FROM dz_connections
 WHERE right IN slides
 )
 
-SELECT dz_nodes.name, substr(slide_nodes.name,length('csmet/cs342/slides/') + 1) AS slide_name FROM dz_todo
+SELECT
+row_number() OVER () as rownum,
+dz_nodes.name,
+substr(slide_nodes.name,length('csmet/cs342/slides/') + 1)
+AS slide_name,
+(SELECT group_concat(value, ' ') FROM json_each(lines)) as desc
+FROM dz_todo
 INNER JOIN dz_nodes ON dz_todo.node = dz_nodes.id
 INNER JOIN slide_connections ON dz_todo.node = slide_connections.left
 INNER JOIN dz_nodes AS slide_nodes ON slide_connections.right = slide_nodes.id
+INNER JOIN dz_lines ON dz_lines.node = dz_todo.node
 WHERE dz_todo.node in (SELECT left FROM slide_connections)
 ORDER BY slide_name
 ;

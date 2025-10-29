aa6b11aa99a8e85c5859a784dbc738f53a253df8
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-10-29 12:37:59 -0400

    timelog

--- tools/orphan_comment_nodes.sh
+++ tools/orphan_comment_nodes.sh
@@ -17,6 +17,6 @@ SELECT day, title, lz_connections.id, node FROM lz_connections
 INNER join lz_entries on lz_entries.id = lz_connections.id
 WHERE node NOT in
 (SELECT node FROM lz_connections
-JOIN dz_nodes on dz_nodes.name = node)
+JOIN dz_nodes on dz_nodes.name LIKE node)
 ;
 EOM

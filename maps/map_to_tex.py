b7ffcbc05d05e95fb89109b37e8aa84a82c231e6
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-12-23 13:25:19 -0500

    initial namespace grouping

--- maps/map_to_tex.py
+++ maps/map_to_tex.py
@@ -2,6 +2,7 @@
 import json
 import sys
 from pprint import pprint
+from collections import deque
 
 obj = None
 
@@ -101,13 +102,111 @@ def display(name, node):
     print("}")
     bigskip()
 
+# generate subgraph trie
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.path = "/"
+        self.terminal = False
+        self.count = 0
+
+
+    def append(self, name):
+        subnames = name.split("/")
+        node = self
+        curpath = []
+        for sn in subnames:
+            curpath.append(sn)
+            if sn not in node.children:
+                child = Trie()
+                child.path = "/".join(curpath)
+                node.children[sn] = child
+            node.count += 1
+            node = node.children[sn]
+        node.terminal = True
+
+
+def hpair(a, b):
+    return a + " \\leaders\\hbox to 10pt {\\hss.\\hss} " + " \\hfill " + b 
+    
+def node_index_ref(name):
+    #return escape(name) + " \\hfill " + xref(name)
+    return hpair(escape(name), xref(name))
+
+def node_index_ref_abbr(name):
+    return hpair(basename(name), xref(name))
+
+namespaces = Trie()
+
+for name in names: namespaces.append(name)
+
+class NamespaceIndex:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+        self.nodes = []
+
+    def display(self):
+        if len(self.nodes) == 0 and len(self.children) == 0: return
+        print(escape(self.name))
+        print("\\par\\begingroup")
+        leftskip("2em")
+
+        smallskip()
+        for child in self.children:
+            # TODO: create page index
+            print(basename(child))
+            smallskip()
+
+        medskip()
+        for node in self.nodes:
+            # print(escape(node))
+            print(node_index_ref_abbr(f"{self.name}/{node}"))
+            smallskip()
+
+        print("\\par\\endgroup")
+        bigskip()
+
+
+def mknsidx(namespaces):
+    q = deque()
+
+    q.append(namespaces)
+
+    nsidx = []
+
+    while q:
+        node = q.popleft()
+        ni = NamespaceIndex(node.path)
+        children = []
+        nodes = []
+        for (k, v) in node.children.items():
+            if v.terminal:
+                nodes.append(k)
+            if (v.count > 0): children.append(v.path)
+            q.append(v)
+        ni.nodes = nodes
+        ni.children = children
+        nsidx.append(ni)
+
+    return nsidx
+
+nsidx = mknsidx(namespaces)
+
 print("\\input eplain")
+
+# print namespace index
+
+for ni in nsidx: ni.display()
+
+# print index
 for name in names:
-    print(escape(name), "\\hfill", xref(name))
+    print(node_index_ref(name))
     smallskip()
 
 print("\\vfill \\break")
 
+# print nodes
 for name in names:
     display(name, nodes[name])
 

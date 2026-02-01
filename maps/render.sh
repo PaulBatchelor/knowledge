880dc1da5cf05c14985cc4edf824ea20c30754d2
Paul Batchelor <thisispaulbatchelor@gmail.com>
2026-02-01 17:43:55 -0500

    update tex files

--- maps/render.sh
+++ maps/render.sh
@@ -4,5 +4,5 @@ while read -r LINE
 do
     MAP=$(echo $LINE | cut -d ':' -f 1)
     echo $MAP
-    cd maps; pdftex $MAP; cd -
+    cd maps; pdftex $MAP 1>/dev/null; cd - > /dev/null
 done < maps/maps.txt

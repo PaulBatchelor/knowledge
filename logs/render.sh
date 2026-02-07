f7e36198d3f8c00331e0e2fd9515c649bb3b762b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2026-02-07 12:01:35 -0500

    update generated log tex files. make log generator less verbose

--- logs/render.sh
+++ logs/render.sh
@@ -3,5 +3,12 @@ while read -r LINE
 do
     NAME=$(echo $LINE | cut -d ':' -f 1)
     echo $NAME
-    cd logs/tex; pdftex $NAME; cd -
+    cd logs/tex;
+    pdftex $NAME >/dev/null;
+    if [ ! "$?" -eq 0 ]
+    then
+        echo "TeX Error. See logs"
+        exit 1
+    fi
+    cd - >/dev/null
 done < logs/logs.txt

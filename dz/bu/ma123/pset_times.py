e696d616c307aa804e5707e28e0c52c3160d000c
Paul Batchelor <thisispaulbatchelor@gmail.com>
2026-04-12 15:20:23 -0400

    update pset data parser

--- dz/bu/ma123/pset_times.py
+++ dz/bu/ma123/pset_times.py
@@ -18,17 +18,45 @@ def sec_to_ts(sec):
     seconds = sec
     return ":".join([str(x) for x in [hours, minutes, seconds]])
 
+# OLD
 pset_time = 0
 review_time = 0
 
+# NEW: times by section
+cursec = None
+ptimes = {}
+rtimes = {}
+sections = set()
+
 for line in sys.stdin:
+    if line[0] == "s":
+        args = line.split(" ")
+        cursec = args[1].replace("\n", "")
+        if cursec not in ptimes:
+            ptimes[cursec] = 0
+        if cursec not in rtimes:
+            rtimes[cursec] = 0
+        if cursec not in sections:
+            sections.add(cursec)
+
     if line[0] == "t" or line[0] == "r":
         args = line.split(" ")
         if args[0] == "t":
             pset_time += ts_to_sec(args[4])
+            ptimes[cursec] += ts_to_sec(args[4])
         if args[0] == "r":
             review_time += ts_to_sec(args[4])
+            rtimes[cursec] += ts_to_sec(args[4])
 
-print(f"pset time: {sec_to_ts(pset_time)}")
-print(f"pset review time: {sec_to_ts(review_time)}")
-print(f"total pset times: {sec_to_ts(review_time + pset_time)}")
+# print(f"pset time: {sec_to_ts(pset_time)}")
+# print(f"pset review time: {sec_to_ts(review_time)}")
+# print(f"total pset times: {sec_to_ts(review_time + pset_time)}")
+
+for sec in sorted(sections):
+    pset_time = ptimes[sec]
+    review_time = rtimes[sec]
+    print(sec)
+    print(f"pset time: {sec_to_ts(pset_time)}")
+    print(f"pset review time: {sec_to_ts(review_time)}")
+    print(f"total pset times: {sec_to_ts(review_time + pset_time)}")
+    print()
